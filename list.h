3c40386c45c93039888a6fa7a585ee4bbba1e3c6
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-10-27 20:20:30 +0300

    realized struct member free pointing to first empty element in list, update functions

--- list.h
+++ list.h
@@ -3,13 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
-typedef int elem_t;
+typedef int list_elem_t;
 
-static const int START = 1;
-static const int STATIC_ELEM = -1;
-static const int POISON = 0xDEADBEEF:
-static const int ERRNO = 0;
+static const int INITIAL = 0;
+static const int NULL_ELEM = 0;
+static const int EMPTY = -1;
+static const int POISON = 0xDEADBEEF;
+static int ERROR = 0;
 
 enum errors
 {
@@ -17,6 +19,13 @@ enum errors
     LIST_INCORREST_PUT_PLACE = 0x1 << 1,
     LIST_INCORRECT_CAPACITY  = 0x1 << 2,
     LIST_INCORRECT_POP_PLACE = 0x1 << 3
+};
+
+struct List_elem
+{
+    list_elem_t data = 0;
+    int next = 0;
+    int prev = 0;
 };
 
 struct List
@@ -30,86 +39,108 @@ struct List
     List_elem *elems = nullptr;
 };
 
-struct List_elem
-{
-    elem_t data = 0;
-    int next = 0;
-    int prev = 0;
-};
 
-void list_ctor (List *list, int capacity, int *err = &ERRNO);
-void fill_list (List *list, int *err);
-void list_dtor (List *list, int *err = &ERRNO);
-void list_insert (List *list, int put_place, elem_t value, int *err = &ERRNO);
+void list_ctor (List *list, int capacity, int *err = &ERROR);
+void fill_list (List *list, int start, int *err);
+void list_dtor (List *list, int *err = &ERROR);
+int list_insert (List *list, int put_place, list_elem_t value, int *err = &ERROR);
 void list_error (List *list, int *err);
-elem_t list_pop (List *list, int pop_place, int *err);
+list_elem_t list_pop (List *list, int pop_place, int *err = &ERROR);
 
-void list_ctor (List *list, int capacity)
+void list_ctor (List *list, int capacity, int *err)
 {
+    assert (list);
+
     if (capacity <= 0)
     {
         *err |= LIST_INCORRECT_CAPACITY;
         list_error (list, err);
     }
-    list->elems = (List_elem *)calloc (capacity, sizeof (List_elem)); //check if nullptr
 
-    fill_list (list);
+    list->elems = (List_elem *)calloc (capacity, sizeof (List_elem)); //check if nullptr
+    assert (list->elems);
+
+    list->capacity = capacity;
+
+    list->head = list->tale = list->size = INITIAL;
+
+    list->elems[NULL_ELEM].data = list->elems[NULL_ELEM].prev = list->elems[NULL_ELEM].next = INITIAL;
+
+    list->free = 1;
+
+    fill_list (list, INITIAL + 1, err);
 }
 
-void list_insert (List *list, int put_place, elem_t value, int *err)
+int list_insert (List *list, int put_place, list_elem_t value, int *err)
 {
-    int index = find_space(list);
+    assert (list);
+    assert (err);
+/*
+    if (list->size == list->capacity)
+    {
+        list_realloc (list);
+    }*/
+
+
+    int insert_index = list->free;
+    printf ("insert_index %d", insert_index);
 
     if (put_place < 0)
     {
         *err |= LIST_INCORREST_PUT_PLACE;
         list_error (list, err);
 
-        break;
+        return POISON;
     }
-    if (!(list->head))
+    else if (!(list->head))
     {
         list->head = list->tale = put_place + 1;
-        list->elems[list->head].prev = 0;
+        list->elems[list->head].prev = NULL_ELEM;
         list->elems[list->head].data = value; //check
     }
     else if (!(put_place))
     {
-        list->elems[index].data = value;
-        list->elems[index].next = list->head;
 
-        list->elems[list->head].prev = index;
+        list->elems[insert_index].data = value;
+        list->elems[insert_index].next = list->head;
 
-        list->head = index;
+        list->elems[list->head].prev = insert_index;
+
+        list->head = insert_index;
     }
-    else if (list->elems[put_place] == POISON)
+    else if (list->elems[put_place].data == POISON)
     {
-        *err |= INSERT_ERROR;
+        *err |= LIST_INSERT_ERROR;
 
         list_error (list, err);
 
-        break;
+        return POISON;
     }
     else if (put_place == list->tale)
     {
-        list->elems[index].data = value;
-        list->elems[index].prev = list->tale;
+        list->elems[insert_index].data = value;
+        list->elems[insert_index].prev = list->tale;
 
-        list->tale = index;
+        list->tale = insert_index;
     }
     else
     {
-        list->elems[index].data = value;
-        list->elems[index].prev = put_place;
-        list->elems[index].next = list->elems[put_place].next;
+        list->elems[insert_index].data = value;
+        list->elems[insert_index].prev = put_place;
+        list->elems[insert_index].next = list->elems[put_place].next;
 
-        list->elems[put_place].next = index;
+        list->elems[put_place].next = insert_index;
     }
+
+    list->free = list->elems[list->free].next;//check if 0
+    (list->size)++;
+
+    return insert_index;
 }
 
-elem_t list_pop (List *list, int pop_place, int *err)
+list_elem_t list_pop (List *list, int pop_place, int *err)
 {
-    if (list->elems[put_place].data = POISON)
+    if (list->elems[pop_place].data == POISON)
     {
         fprintf (stderr, "POP_ERROR: incorrect pop place");
 
@@ -124,11 +155,12 @@ elem_t list_pop (List *list, int pop_place, int *err)
     if (pop_place == list->head)
     {
         list->head = list->elems[pop_place].next;
-        list->elems[list->head].prev = 0;
+        list->elems[list->head].prev = NULL_ELEM;
     }
     else if (pop_place == list->tale)
     {
         list->tale = list->elems[pop_place].prev;
+        list->elems[list->tale].next = NULL_ELEM;
     }
     else
     {
@@ -137,11 +169,17 @@ elem_t list_pop (List *list, int pop_place, int *err)
     }
 
     list->elems[pop_place].data = POISON;
-    list->elems[pop_place].next = STATIC_ELEM;
-    list->elems[pop_place].prev = STATIC_ELEM;
+    list->elems[pop_place].next = list->free;
+    list->elems[pop_place].prev = EMPTY;
+
+    list->free = pop_place;
+
+    (list->size)--;
+
+    return return_value;
 }
 
-void list_dtor (List *list)
+void list_dtor (List *list, int *err)
 {
     if (list != nullptr && list->elems != nullptr)
     {
@@ -152,32 +190,38 @@ void list_dtor (List *list)
     }
 }
 
-void fill_list(List *list)
+int list_error (List *list, int *err)
 {
-    int value = START;
-
-    list->elems[0].next = 0;
-    list->elems[0].prev = 0;
-    list->elems[0].data = POISON;
-
-    while (value < LIST_SIZE)
+    do
     {
-        list->elems[value].data = POISON;
-        list->elems[value].next = STATIC_ELEM;
-        list->elems[value].prev = STATIC_ELEM;
+        if (list)
+    }while(0);
 
-        if (value == LIST_SIZE - 1)
-        {
-            list->elems[value].next = 0;
-        }
-        if (value == START)
-        {
-            list->elems[value].prev = 0;
-        }
-
-        value++;
-    }
 }
 
+void list_error (List *list, int *err)
+{
+    printf ("ERROR");
+}
+
+void fill_list (List *list, int start, int *err)
+{
+    while (start < list->capacity)
+    {
+        list->elems[start].data = POISON;
+        list->elems[start].prev = EMPTY;
+
+        if (start == list->capacity - 1)
+        {
+            list->elems[start].next = NULL_ELEM;
+        }
+        else
+        {
+            list->elems[start].next = start + 1;
+        }
+
+        start++;
+    }
+}
 
 #endif /* LIST_H */
