3bac85f55f3e72285e98042eddefc98e49a399c8
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-10-27 22:02:18 +0300

    add list error, list dump, canaries fix bug with changing free at insert

--- list.h
+++ list.h
@@ -5,20 +5,41 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include "../ptr_check.h"
+#undef ERROR
+static int ERROR = 0;
+
+#define CHECK(case, action) \
+if (case)                   \
+{                           \
+    action                  \
+}
+
 typedef int list_elem_t;
+typedef unsigned long long canary_t;
+
 
 static const int INITIAL = 0;
 static const int NULL_ELEM = 0;
 static const int EMPTY = -1;
 static const int POISON = 0xDEADBEEF;
-static int ERROR = 0;
+static const canary_t CANARY = 0xAB8EACAAAB8EACAA;
+
+static FILE *list_log = fopen ("list_log.txt", "w");
 
 enum errors
 {
-    LIST_INSERT_ERROR        = 0x1 << 0,
-    LIST_INCORREST_PUT_PLACE = 0x1 << 1,
-    LIST_INCORRECT_CAPACITY  = 0x1 << 2,
-    LIST_INCORRECT_POP_PLACE = 0x1 << 3
+    LIST_BAD_READ_LIST          = 0x1 << 0,
+    LIST_BAD_READ_DATA          = 0x1 << 1,
+    LIST_INCORRECT_SIZE         = 0x1 << 2,
+    LIST_INCORRECT_CAPACITY     = 0x1 << 3,
+    LIST_INSERT_ERROR           = 0x1 << 4,
+    LIST_INCORRECT_INSERT_PLACE = 0x1 << 5,
+    LIST_INCORRECT_POP_PLACE    = 0x1 << 6,
+    LIST_POP_FROM_EMPTY_LIST    = 0x1 << 7,
+    LIST_PREV_NEXT_OP_ERR       = 0x1 << 8,
+    LIST_FREE_ELEM_NOT_EMPTY    = 0x1 << 9,
+    LIST_VIOLATED_LIST        = 0x1 << 10
 };
 
 struct List_elem
@@ -30,6 +51,10 @@ struct List_elem
 
 struct List
 {
+    #ifdef CANARY_PROT
+    canary_t left_canary = CANARY;
+    #endif
+
     int head = 0;
     int tale = 0;
     int size = 0;
@@ -37,6 +62,10 @@ struct List
     int capacity = 0;
 
     List_elem *elems = nullptr;
+
+    #ifdef CANARY_PROT
+    canary_t right_canary = CANARY;
+    #endif
 };
 
 
@@ -44,8 +73,12 @@ void list_ctor (List *list, int capacity, int *err = &ERROR);
 void fill_list (List *list, int start, int *err);
 void list_dtor (List *list, int *err = &ERROR);
 int list_insert (List *list, int put_place, list_elem_t value, int *err = &ERROR);
-void list_error (List *list, int *err);
+int list_error (List *list, int *err);
 list_elem_t list_pop (List *list, int pop_place, int *err = &ERROR);
+void list_dump (List *list, int *err);
+void dump_list_members (List *list, int *err);
+void dump_elems (List *list, int *err);
+void dump_list_errors (List *list, int *err);
 
 void list_ctor (List *list, int capacity, int *err)
 {
@@ -69,6 +102,8 @@ void list_ctor (List *list, int capacity, int *err)
     list->free = 1;
 
     fill_list (list, INITIAL + 1, err);
+
+    list_error (list, err);
 }
 
 int list_insert (List *list, int put_place, list_elem_t value, int *err)
@@ -81,14 +116,19 @@ int list_insert (List *list, int put_place, list_elem_t value, int *err)
         list_realloc (list);
     }*/
 
+    list_error (list, err);
 
     int insert_index = list->free;
-    printf ("insert_index %d", insert_index);
+
+    int previous_free = list->free;
+    list->free = list->elems[previous_free].next;//check if 0
 
     if (put_place < 0)
     {
-        *err |= LIST_INCORREST_PUT_PLACE;
+        *err |= LIST_INCORRECT_INSERT_PLACE;
         list_error (list, err);
+
+        list->free = previous_free;
 
         return POISON;
     }
@@ -103,6 +143,7 @@ int list_insert (List *list, int put_place, list_elem_t value, int *err)
 
         list->elems[insert_index].data = value;
         list->elems[insert_index].next = list->head;
+        list->elems[insert_index].prev = NULL_ELEM;
 
         list->elems[list->head].prev = insert_index;
 
@@ -113,6 +154,8 @@ int list_insert (List *list, int put_place, list_elem_t value, int *err)
         *err |= LIST_INSERT_ERROR;
 
         list_error (list, err);
+
+        list->free = previous_free;
 
         return POISON;
     }
@@ -132,8 +175,9 @@ int list_insert (List *list, int put_place, list_elem_t value, int *err)
         list->elems[put_place].next = insert_index;
     }
 
-    list->free = list->elems[list->free].next;//check if 0
     (list->size)++;
+
+    list_error (list, err);
 
     return insert_index;
 }
@@ -149,6 +193,17 @@ list_elem_t list_pop (List *list, int pop_place, int *err)
 
         return POISON;
     }
+    if (list->size == 0)
+    {
+        fprintf (stderr, "POP_ERROR: pop from empty list");
+
+        *err |= LIST_POP_FROM_EMPTY_LIST;
+        list_error (list, err);
+
+        return POISON;
+    }
+
+    list_error (list, err);
 
     int return_value = list->elems[pop_place].data;
 
@@ -176,6 +231,8 @@ list_elem_t list_pop (List *list, int pop_place, int *err)
 
     (list->size)--;
 
+    list_error (list, err);
+
     return return_value;
 }
 
@@ -188,21 +245,109 @@ void list_dtor (List *list, int *err)
         list->elems = nullptr;
         list = nullptr;
     }
+    else
+    {
+        printf ("\nnothing to dtor\n");
+    }
 }
 
 int list_error (List *list, int *err)
 {
+    int index = list->head;
+    int counter = 0;
+
     do
     {
-        if (list)
+        CHECK (is_bad_read_ptr (list), {*err |= LIST_BAD_READ_LIST;
+                                 break;})
+        CHECK (is_bad_read_ptr (list->elems), {*err |= LIST_BAD_READ_DATA;
+                                 break;})
+        CHECK (list->size < 0, *err |= LIST_INCORRECT_SIZE;)
+        CHECK (list->capacity <= 0, *err |= LIST_INCORRECT_CAPACITY;)
+
+        if (!(*err & LIST_INCORRECT_SIZE) && !(*err & LIST_INCORRECT_CAPACITY))
+        {
+            while (counter++ < list->size - 1)
+            {
+                CHECK (list->elems[list->elems[index].next].prev != index, *err |= LIST_PREV_NEXT_OP_ERR;)
+                index = list->elems[index].next;
+            }
+
+            counter = 0;
+            index = list->free;
+
+            while (counter++ < list->capacity - list->size - 1)
+            {
+                CHECK (list->elems[index].prev != EMPTY, *err |= LIST_FREE_ELEM_NOT_EMPTY;)
+            }
+        }
+
+        #ifdef CANARY_PROT
+        CHECK (list->left_canary != CANARY || list->right_canary != CANARY, *err |= LIST_VIOLATED_LIST;)
+        #endif
     }while(0);
 
+    list_dump (list, err);
 }
 
-void list_error (List *list, int *err)
+void list_dump (List *list, int *err)
 {
-    printf ("ERROR");
+    fprintf (list_log, "list\n");
+
+    dump_list_members (list, err);
+    dump_list_errors  (list, err);
+
+    fprintf (list_log, "\n\n\n\n\n");
 }
+
+void dump_list_members (List *list, int *err)
+{
+    fprintf (list_log, "list head is %d\nlist tale is %d\n"
+                       "list size id %d\nlist capacity id %d\n"
+                       "list free is %d\nlist elems is [%p]\n",
+                       list->head, list->tale, list->size, list->capacity, list->free, list->elems);
+    dump_elems (list, err);
+}
+
+void dump_elems (List *list, int *err)
+{
+    for (int index = 0; index < list->capacity; index++)
+    {
+        fprintf (list_log, "idx[%d]\t data [%d]\t next is [%d]\t prev is [%d]\n", index, list->elems[index].data, list->elems[index].next, list->elems[index].prev);
+    }
+}
+
+void dump_list_errors (List *list, int *err)
+{
+    const char *status[10] = {};
+
+    if (*err & LIST_BAD_READ_LIST)
+    {
+        fprintf (list_log, "list is a bad ptr");
+    }
+    if (*err & LIST_BAD_READ_DATA)
+    {
+        fprintf (list_log, "list elems is a bad ptr");
+    }
+    if (*err & LIST_INCORRECT_CAPACITY)
+    {
+        fprintf (list_log, "capacity is incorrect (<=0)");
+    }
+    if (*err & LIST_PREV_NEXT_OP_ERR)
+    {
+        fprintf (list_log, "next element of previous is not equal to original");
+    }
+    if (*err & LIST_FREE_ELEM_NOT_EMPTY)
+    {
+        fprintf (list_log, "free element is not empty");
+    }
+    if (*err & LIST_VIOLATED_LIST)
+    {
+        fprintf (list_log, "access rights of stack are invaded");
+    }
+
+}
+
 
 void fill_list (List *list, int start, int *err)
 {
